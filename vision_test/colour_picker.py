724c5a8cf71f97c0b17b950598587a7014f8f3a2
Joshua Riddell <joshua.riddell11@gmail.com>
2016-06-09 15:40:46 +1000

    Add click selection to colour picker

--- vision_test/colour_picker.py
+++ vision_test/colour_picker.py
@@ -3,29 +3,47 @@ import numpy as np
 import time
 import cv2
 
-def mouse_click(event, x, y, flags, param):
-    
+KERNEL_SIZE = 10
+
+space = [[], [], []]
+bounds = [np.array([0,0,0]), np.array([255,255,255])]
+
+def mouse_click(frame, event, x, y):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)[y][x]
+        for i in range(3):
+            space[i].append(hsv[i])
+
+        bounds[0] = np.array([min(space[i]) for i in range(3)])
+        bounds[1] = np.array([max(space[i]) for i in range(3)])
+
+        cv2.imshow('colour', np.array([np.array([frame[y][x] for i in range(100)]) for i in range(100)]))
+
 
 cv2.namedWindow('frame')
-cv2.setMouseCallback('frame', mouse_click)
+cv2.setMouseCallback('frame', lambda event, x, y, flags, param: mouse_click(frame, event, x, y))
 
 cap = cv2.VideoCapture(1)
 
-cap.set(3, 1920)
-cap.set(4, 1080)
+# cap.set(3, 1920)
+# cap.set(4, 1080)
 
 frame = cap.read()[1]
 
 while(True):
     ret, frame = cap.read()
-    frame = cv2.boxFilter(frame, -1, (10,10))
+    frame = cv2.boxFilter(frame, -1, (KERNEL_SIZE, KERNEL_SIZE))
+
+    thresh = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    thresh = cv2.inRange(thresh, *bounds)
+
     if ret==True:
         cv2.imshow('frame',frame)
+        cv2.imshow('thresh',thresh)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
 
-# Release everything if job is finished
 cap.release()
 cv2.destroyAllWindows()
