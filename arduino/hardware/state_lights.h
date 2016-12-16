5982cf285088d82a9a0dc493f50207162d18375b
Joshua Riddell <jridd30@gmail.com>
2016-12-16 23:58:22 +1000

    Add support for status lights

---
+++ arduino/hardware/state_lights.h
@@ -0,0 +1,14 @@
+#ifndef STATE_LIGHTS_
+#define STATE_LIGHTS_
+
+// define pins
+#define WHITE 14
+#define BLUE 15
+#define GREEN 16
+#define RED 17
+
+#define NUM_LIGHTS 4
+
+const byte lights[] = { WHITE, BLUE, GREEN, RED };
+
+#endif  // STATE_LIGHTS_
---
+++ arduino/hardware/state_lights.ino
@@ -0,0 +1,36 @@
+#include "state_lights.h"
+
+void setup_lights() {
+  for (int n = 0; n < 3; ++n) {
+    strum_lights();
+  }
+
+  set_light(WHITE);
+}
+
+void strum_lights() {
+  for (int i = 0; i < NUM_LIGHTS; ++i) {
+    pinMode(lights[i], OUTPUT);
+    digitalWrite(lights[i], HIGH);
+    delay(30);
+  }
+
+  for (int i = 0; i < NUM_LIGHTS; ++i) {
+    pinMode(lights[i], OUTPUT);
+    digitalWrite(lights[i], LOW);
+    delay(30);
+  }
+}
+
+void set_light(int pin) {
+  digitalWrite(pin, HIGH);
+}
+
+void clear_light(int pin) {
+  digitalWrite(pin, LOW);
+}
+
+void toggle_light(int pin) {
+  digitalWrite(pin, !digitalRead(pin));
+}
+
