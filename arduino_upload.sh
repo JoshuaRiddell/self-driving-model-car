07489af5c8885095aaa9b5509bc456d9ec561b8a
JoshuaRiddell <jridd30@gmail.com>
2017-06-19 13:36:14 +0000

    Remove relative path in upload script

--- arduino_upload.sh
+++ arduino_upload.sh
@@ -1,1 +1,1 @@
-sudo arduino --upload --board arduino:avr:uno --port /dev/ttyACM0 ../hardware/hardware.ino
+sudo arduino --upload --board arduino:avr:uno --port /dev/ttyACM0 ~/car/hardware/hardware.ino
