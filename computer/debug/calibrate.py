0586ab635b15010ddf820885403d7148801f0e1e
JoshuaRiddell <jridd30@gmail.com>
2017-06-21 01:52:34 +0000

    Add comments to calibration scripts

--- computer/debug/calibrate.py
+++ computer/debug/calibrate.py
@@ -1,48 +1,59 @@
+# uses chessboard to find a perspecive transform matrix
+
 import numpy as np
 import cv2
 import glob
 
-# termination criteria
+# termination criteria for sub pixel corner detection
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+# size of the chessboard
 size = (7,5)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((size[0]*size[1],3), np.float32)
 objp[:,:2] = np.mgrid[0:size[0],0:size[1]].T.reshape(-1,2)
 
+# camera resolution to use
 resolution = (1920, 1080)
 
+# setup camera
 cam = cv2.VideoCapture(0)
 cam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1920);
 cam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1080);
 
-
+# loop indefinitely for continuous camera feed
 while True:
+    # get a frame and convert to grayscale
     ret, img = cam.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Find the chess board corners
+    # find the chess board corners
     ret, corners = cv2.findChessboardCorners(gray, size, flags=cv2.CALIB_CB_FAST_CHECK)
 
-    # If found, add object points, image points (after refining them)
+    # if found, add object points, image points (after refining them)
     if ret == True:
+        # get chessboard corners with sub pixel accuracy
         ret, corners = cv2.findChessboardCorners(gray, size, flags=cv2.CALIB_CB_ADAPTIVE_THRESH)
         cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
 
+        # perpare arrays for mapping between chessboard and rectangle
         rectangle = np.array([corners[x] for x in (-7, 0, -1, 6)])
         dest = np.array([[670, 450], [1250, 450], [670, 970], [1250, 970]], dtype=np.float32)
         print rectangle
 
+        # get the perspective matrix
         M = cv2.getPerspectiveTransform(rectangle, dest)
         warped = cv2.warpPerspective(img, M, resolution)
 
+        # draw the chessboard corners on the image and display the warped image
         cv2.drawChessboardCorners(img, size, corners, ret)
-
         cv2.imshow('warped', warped)
 
+    # show non-warped image
     cv2.imshow('img', img)
 
+    # wait for 100ms or a key
     cv2.waitKey(100)
 
 cv2.destroyAllWindows()
--- computer/debug/calibrate_manual.py
+++ computer/debug/calibrate_manual.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import glob
 
+# file to store the finished matrix in
 MATRIX_FILENAME = '../perspective_matrix.txt'
 
 # sub pixel corner algorithm
@@ -17,9 +18,9 @@ OUT_RES = (1800, 1280)
 SCALE = 0.1
 
 # mapping from sqare test strip to output
-HOR_CENT = OUT_RES[0]/2
-INC = OUT_RES[0]/4 * SCALE
-VER_CENT = OUT_RES[1] - INC*2
+HOR_CENT = OUT_RES[0]/2  # horizontal centreline
+INC = OUT_RES[0]/4 * SCALE  # increment (how big the square will be)
+VER_CENT = OUT_RES[1] - INC*2  # vertical centreline
 MAP_TO = np.array((
     (HOR_CENT - INC, VER_CENT - INC),
     (HOR_CENT + INC, VER_CENT - INC),
@@ -28,23 +29,26 @@ MAP_TO = np.array((
 ), np.float32)
 
 # mouse clicking globals
-clicked = False
-coordinate = (0, 0)
+clicked = False  # mouse button was clicked
+coordinate = (0, 0)  # coordinate of mouse click
 
 def setup_camera():
+    "Setup the camera with chosen resolution"
     cam = cv2.VideoCapture(0)
     cam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, RES[0]);
     cam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, RES[1]);
     return cam
 
 def get_image(cam):
+    "Get an image from the camera"
     ret, img = cam.read()
     if not ret:
-        return ret
+        return None
     else:
         return img
 
 def handle_mouse(event, x, y, flags, param):
+    "Handle mouse event, set a flag for button pressed and coordinate."
     global clicked
     global coordinate
 
@@ -55,23 +59,26 @@ def handle_mouse(event, x, y, flags, param):
         clicked = 2
 
 def main():
-    global coordinate
     global clicked
+    global coordinate
 
     cam = setup_camera()
 
+    # make window and attach callback
     cv2.namedWindow('main')
     cv2.setMouseCallback('main', handle_mouse)
 
-    coords = [];
-    genMatrix = False
-    M = None
+    coords = [];  # array of coordiantes for rectangle
+    genMatrix = False  # flag to generate matrix on next loop
+    M = None  # perspecitve matrix
 
+    # try and load a previously made coordinate array
     try:
         fd = open(MATRIX_FILENAME, 'r')
 
         coords = eval(fd.readline())
 
+        # build the perspecitve matrix based on coordinates
         M = cv2.getPerspectiveTransform(
                 np.array(coords, np.float32),
                 MAP_TO);
@@ -83,35 +90,47 @@ def main():
         M = None
         pass
 
+    # frame loop for user interface
     while True:
+        # get image from camera
         img = get_image(cam)
-        if not img.any():
+        if img is None:
             continue
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+        # if left mouse was clicked then add coordinate to coordinates array
         if clicked == 1:
             if len(coords) == 4:
+                # if we have enough coordinates then make a new matrix
                 genMatrix = True
             else:
+                # add coordinates to array
                 coords.append(coordinate)
                 print (len(coords))
+
+            # reset clicked flag
             clicked = False
+        # if right mouse was clicked
         elif clicked == 2:
             if len(coords) > 0:
+                # cancel last coordinate
                 coords.pop()
                 print (len(coords))
+
+            # reset clicked flag
             clicked = False
 
-        # print clicked coords
+        # display clicked coords on frame
         for coord in coords:
             cv2.circle(img, tuple(coord), 10, (255, 0, 0), -1)
 
+        # generate new matrix if flag was set
         if genMatrix:
+            # gen matrix
             M = cv2.getPerspectiveTransform(np.array(coords, np.float32), MAP_TO)
 
+            # print it and save it to a file of format
+            # (coordinates,mapping_coords,input_resolution,output_resolution)
             print M
-
             fd = open(MATRIX_FILENAME, 'w')
             fd.write(str(list([list(x) for x in coords])))
             fd.write("\n")
@@ -122,14 +141,19 @@ def main():
             fd.write(str(list(OUT_RES)))
             fd.close()
 
+            # reset coordinates array and flags
             coords = []
             genMatrix = False;
 
+        # show undistorted image
         cv2.imshow('main', img)
+
+        # if we have a matrix then show distorted image
         if M is not None:
             warped = cv2.warpPerspective(img, M, OUT_RES)
             cv2.imshow('warped', warped)
 
+        # wait for a key or 10ms to pass
         cv2.waitKey(10)
 
 if __name__ == '__main__':
