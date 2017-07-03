44da092daa4d64f98168c46f11f053e4ca537533
Joshua Paul  Riddell <s4394724@student.uq.edu.au>
2017-07-04 00:22:58 +1000

    Add switch for networking

--- computer/main.py
+++ computer/main.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python2
 
+USE_SERVER = False
+
 import vision as vi
 import hardware as hi
 from threading import Thread, Lock
-import server
 from time import sleep
+if USE_SERVER:
+    import server
 
 class Main(Thread):
     """Main control thread.
     """
-
     def __init__(self):
         """Initialise the vision interface and hardware interface (arduino and
         camera).
         """
         super(Main, self).__init__()
-        self.daemon = True
+        self.daemon = not USE_SERVER
 
         self.vision = vi.VisionInterface()
         self.hardware = hi.HardwareInterface()
@@ -34,8 +36,9 @@ if __name__ == "__main__":
     main = Main()
     main.start()
 
-    # make the frame streaming server. This gets main control flow.
-    server.register_vision(main.vision)
-    server = server.WebServer(('0.0.0.0', 5000), server.CamHandler)
-    server.serve_forever()
+    if USE_SERVER:
+        # make the frame streaming server. This gets main control flow.
+        server.register_vision(main.vision)
+        server = server.WebServer(('0.0.0.0', 5000), server.CamHandler)
+        server.serve_forever()
 
